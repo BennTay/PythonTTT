95f1540617a44aaffb4cab6288f9bae833f27d1a
BennTay <benntayguobin@gmail.com>
2019-07-07 04:14:13 +0800

    Implement graphic game in progress

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,102 @@
 from graphics import *
 
+class GraphicGame:
+    def __init__(self, plain_game, window):
+        self.plain_game = plain_game
+        self.window = window
+        self.pointList = []
+        self.pointList.append(Point(100, 200))
+        self.pointList.append(Point(300, 200))
+        self.pointList.append(Point(500, 200))
+        self.pointList.append(Point(100, 400))
+        self.pointList.append(Point(300, 400))
+        self.pointList.append(Point(500, 400))
+        self.pointList.append(Point(100, 600))
+        self.pointList.append(Point(300, 600))
+        self.pointList.append(Point(500, 600))
+
+    def updateWindow(self, gridNum):
+        self.pointList[int(gridNum) - 1].draw(self.window)
+
+
+def initializeBoard(window):
+    line1 = Line(Point(0, 300), Point(600, 300))
+    line2 = Line(Point(0, 500), Point(600, 500))
+    line3 = Line(Point(200, 100), Point(200, 700))
+    line4 = Line(Point(400, 100), Point(400, 700))
+    topBorder = Line(Point(0, 100), Point(600, 100))
+    textBox = Text(Point(300, 50), "Tic-Tac-Toe has started!")
+    line1.draw(window)
+    line2.draw(window)
+    line3.draw(window)
+    line4.draw(window)
+    topBorder.draw(window)
+    textBox.draw(window)
+
+def translateCoordinatesToGrid(coordinates):
+    x = coordinates.getX()
+    y = coordinates.getY()
+
+    if y < 100:
+        raise ValueError
+    elif (y <= 300 and y >= 100):
+        if (x <= 200 and x >= 0):
+            return 1
+        elif (x <= 400 and x > 200):
+            return 2
+        elif (x <= 600 and x > 400):
+            return 3
+    elif (y <= 500 and y > 300):
+        if (x <= 200 and x >= 0):
+            return 4
+        elif (x <= 400 and x > 200):
+            return 5
+        elif (x <= 600 and x > 400):
+            return 6
+    elif (y <= 700 and y > 500):
+        if (x <= 200 and x >= 0):
+            return 7
+        elif (x <= 400 and x > 200):
+            return 8
+        elif (x <= 600 and x > 400):
+            return 9
+
 def runWithGraphics():
-    # print('Tic-Tac-Toe with graphics is starting, enjoy!')
-    print('Sorry, this is still a work in progress. Feel free to try without graphics!')
+    window = GraphWin('Tic-Tac-Toe', 600, 700)
+    initializeBoard(window)
+    plain_game = PlainGame()
+    currentGame = GraphicGame(plain_game, window)
+
+
+    for i in range(9):
+        currentGame.plain_game.printRoundInfo()
+        currentGame.plain_game.printGrid()
+        while True:
+            try:
+                #playerGridChoice = input('Player ' + str(((currentGame.roundNum%2)+1)) + ', pick a number.\n')
+                choiceCoordinates = currentGame.window.getMouse()
+                playerGridChoice = str(translateCoordinatesToGrid(choiceCoordinates))
+                if isValidGridChoice(playerGridChoice, currentGame.plain_game.grid): # Consider rewriting this function to take in an int
+                    break
+                else:
+                    raise ValueError
+            except: # Consider refactoring/restructuring
+                print('Invalid choice, please pick an available number.')
+        currentGame.plain_game.update(playerGridChoice)
+        currentGame.updateWindow(playerGridChoice)
+        if currentGame.plain_game.hasAWin(playerGridChoice):
+            loser = currentGame.plain_game.getCurrentPlayerNum()
+            if loser == '1':
+                currentGame.plain_game.winner = '2'
+            else:
+                currentGame.plain_game.winner = '1'
+            break
+    displayWinScreen(currentGame.plain_game)
+
+
+    window.getMouse()
+    window.close()
+
 
 class Grid:
     def __init__(self, sym):
@@ -103,9 +197,9 @@ class PlainGame:
 
     def getCurrentToken(self):
         if self.getCurrentPlayerNum() == '1':
-            return 'X'
-        else:
             return 'O'
+        else:
+            return 'X'
 
     def hasAWin(self, currentGridChoice):
         gridNum = int(currentGridChoice)
@@ -198,7 +292,11 @@ def runWithoutGraphics():
                 print('Invalid choice, please pick an available number.')
         currentGame.update(playerGridChoice)
         if currentGame.hasAWin(playerGridChoice):
-            currentGame.winner = currentGame.getCurrentPlayerNum()
+            loser = currentGame.getCurrentPlayerNum()
+            if loser == '1':
+                currentGame.winner = '2'
+            else:
+                currentGame.winner = '1'
             break
     displayWinScreen(currentGame)
 
